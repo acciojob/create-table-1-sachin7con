167026ec1b07926c915da49bb25cebf4bba74a4a
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-03-19 16:24:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,15 +2,14 @@ function insert_Row() {
     //SGN Write your code here
 	
 	  let elem = document.getElementById("sampleTable");
-		let firstRow = elem.getElementsByTagName('tr')[0];
-		let newRow = document.createElement('tr');
-		let cell1 = document.createElement('td');
-		let cell2 = document.createElement('td');
-		cell1.textContent = "New Cell1";
-		cell2.textContent = "New Cell2";
-		newRow.appendChild(cell1);
-		newRow.appendChild(cell2);
-	
-	    elem.insertBefore(newRow, firstRow);
-  
+    let firstRow = elem.getElementsByTagName('tr')[0];
+    let newRow = document.createElement('tr');
+    let cell1 = document.createElement('td');
+    let cell2 = document.createElement('td');
+    cell1.textContent = "New Cell1";
+    cell2.textContent = "New Cell2";
+    newRow.appendChild(cell1);
+    newRow.appendChild(cell2);
+
+    elem.insertBefore(newRow, elem.children[0]);
 }
