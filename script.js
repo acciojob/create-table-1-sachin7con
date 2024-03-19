c4833a8d9381c0549e06433105b0d3e994b6a444
sachin7con <86023048+sachin7con@users.noreply.github.com>
2024-03-19 16:17:34 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,16 @@
 function insert_Row() {
-    //Write your code here
-  
+    //SGN Write your code here
+	
+	  let elem = document.getElementById("sampleTable");
+		let firstRow = elem.getElementsByTagName('tr')[0];
+		let newRow = document.createElement('tr');
+		let cell1 = document.createElement('td');
+		let cell2 = document.createElement('td');
+		cell1.textContent = "New Cell1";
+		cell2.textContent = "New Cell2";
+		newRow.appendChild(cell1);
+		newRow.appendChild(cell2);
+	
+	    elem.insertBefore(newRow, firstRow);
   
 }
